62e6241fedbd4c6c1a9c5d2f18c8f1647c85c767
Burak-Baylan <67222076+Burak-Baylan@users.noreply.github.com>
2023-04-26 01:20:54 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "3225622cabfc3f9f319e78223f2daa79",
-"index.html": "87b2741d8db5f7e1c7bb6693c5b35bae",
-"/": "87b2741d8db5f7e1c7bb6693c5b35bae",
-"main.dart.js": "678de1535c93dc113a65d1c39a19c342",
+"index.html": "121f3a47bafa620bbf8fe09b06d15cf5",
+"/": "121f3a47bafa620bbf8fe09b06d15cf5",
+"main.dart.js": "37adbc9d53e7ca93e4754cca670a2833",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
